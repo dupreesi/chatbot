e50b67337fa35a24242ca4bd213261cf86483a0b
Simon Dupree <dupreenator@gmail.com>
2019-01-27 10:45:56 +0000

    query updated

--- api/Controllers/getUsers.js
+++ api/Controllers/getUsers.js
@@ -15,22 +15,21 @@ const getUserInfo = (req, res) => {
   User.findOne({ name: userToSearch }, function(err, userExists) {
     if (err) {
       return res.json({
-        //This is the standard format of response for Dialogflow, i.e. Dialogflow will only be able to parse the response if it has these three keys.
-        // speech: 'Something went wrong',
         fulfillmentText: 'Something went wrong',
         source: 'user-info'
       });
     }
     if (userExists) {
       return res.json({
-        fulfillmentText: 'Something went wrong' + userExists.name,
+        fulfillmentText: `Welcome back ${
+          userExists.name
+        }! How was your day at school today?`,
         source: 'user-info'
       });
     } else {
       return res.json({
-        // speech: 'Currently I am not having information about this user',
         fulfillmentText:
-          'Currently I am not having information about this user',
+          'Sorry I cannot find you. Please register first and we can get Started!',
         source: 'user-info'
       });
     }
